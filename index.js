a7df5cb8af921bcd738ad0d70677ff59e93b469f
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-23 01:12:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
-  var eArray = []
-  
+  var muAndInst = []
+  for (var i = 0; i < musicians.length; i++) {
+    muAndInst.push(musicians[i] + 'plays' + instruments[i])
+  }
+  return muAndInst
 }
